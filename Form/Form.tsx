61c197d48f1a1f1497efd4bf320d9479122d1bda
Jon Wainwright <jonw@audentio.com>
2019-05-15 15:41:54 +0100

    Fix convertToDots is not defined

--- Form/Form.tsx
+++ Form/Form.tsx
@@ -1,6 +1,7 @@
 import { passProps } from '@audentio/utils/src/passProps';
 import { throttle } from '@audentio/utils/src/throttle';
 import React, { Component } from 'react';
+import { Value } from 'slate';
 import { FormProps, FormState, FormValue } from './types';
 
 const initialFormState: FormState = {
@@ -9,6 +10,44 @@ const initialFormState: FormState = {
     value: {},
     errors: {},
     touched: {},
+};
+
+export const convertFromDots = value => {
+    const parsedFormValue = {};
+    Object.keys(value).forEach(key => {
+        const splitKey = key.split('.');
+        let currentRef = parsedFormValue;
+        for (let i = 0, len = splitKey.length; i < len; i++) {
+            const currentKey = splitKey[i];
+            if (i + 1 === len) {
+                currentRef[currentKey] = value[key];
+            } else {
+                if (typeof currentRef[currentKey] === 'undefined') {
+                    currentRef[currentKey] = {};
+                }
+                currentRef = currentRef[currentKey];
+            }
+        }
+    });
+
+    return parsedFormValue;
+};
+
+export const convertToDots = (value, ele = {}, prefix = '', depth = 0) => {
+    // don't include arrays
+    if (typeof value === 'object' && value !== null && depth < 5 && !value.length && value instanceof Value === false) {
+        const keys = Object.keys(value);
+
+        keys.forEach(key => {
+            const combinedKey = prefix ? `${prefix}.${key}` : key;
+
+            convertToDots(value[key], ele, combinedKey, depth + 1);
+        });
+    } else if (prefix && prefix.length > 0) {
+        ele[prefix] = value; // eslint-disable-line no-param-reassign
+    }
+
+    return ele;
 };
 
 /**
